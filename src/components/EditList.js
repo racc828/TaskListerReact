711683d07c85bbddd55e113e0e61ffc53d49e43d
github username <github email address>
2017-08-28 21:54:25 -0400

    editing works

---
+++ src/components/EditList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default class EditList extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      listName: this.props.listName
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({listName: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let newListName = this.state.listName
+    this.props.editList(newListName)
+  }
+
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="listName" onChange={this.handleChange} value={this.state.listName}/>
+          <button type="submit">Save </button>
+        </form>
+      </div>
+    )
+  }
+
+}
